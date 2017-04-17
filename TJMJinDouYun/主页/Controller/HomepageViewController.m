98807daab12c8dfd75d1802826e6a092a49d3808
JiamingTu <tujiamingdd@sina.com>
2017-04-17 18:33:12 +0800

    主页小小设置

--- TJMJinDouYun/主页/Controller/HomepageViewController.m
+++ TJMJinDouYun/主页/Controller/HomepageViewController.m
@@ -17,6 +17,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    
+}
+#pragma  mark - 设置导航左右按钮 
+- (UIBarButtonItem *)setNaviItemWithImageName:(NSString *)imageName tag:(int)tag {
+    UIImage *itemImage = [UIImage imageNamed:@""];
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setBackgroundImage:itemImage forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(itemAction:) forControlEvents:UIControlEventTouchUpInside];
+    UIView *itemView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, itemImage.size.width, itemImage.size.height)];
+    [itemView addSubview:button];
+    UIBarButtonItem *item = [[UIBarButtonItem alloc]initWithCustomView:itemView];
+    return item;
 }
 
 - (void)didReceiveMemoryWarning {
