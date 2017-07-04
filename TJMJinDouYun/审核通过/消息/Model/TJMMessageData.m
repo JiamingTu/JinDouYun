061a7aa90d2bb0a6d567b2a5c901a4c105c0d47b
JiamingTu <tujiamingdd@sina.com>
2017-07-04 10:01:50 +0800

    遗漏提交

---
+++ TJMJinDouYun/审核通过/消息/Model/TJMMessageData.m
@@ -0,0 +1,50 @@
+//
+//  TJMMessageData.m
+//  TJMJinDouYun
+//
+//  Created by Jiaming Tu on 2017/6/5.
+//  Copyright © 2017年 zhongzhichuangying. All rights reserved.
+//
+
+#import "TJMMessageData.h"
+
+@implementation TJMMessageData
+
++ (NSDictionary *)mj_objectClassInArray {
+    return @{@"data":@"TJMMessageModel"};
+}
+
+@end
+
+@implementation TJMMessageModel
+
++ (NSDictionary *)mj_objectClassInArray {
+    return @{@"receivers":@"TJMReceiver"};
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    if (self = [super init]) {
+        [self mj_decode:aDecoder];
+    }
+    return self;
+}
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+    [self mj_encode:aCoder];
+}
+
+
+@end
+
+@implementation TJMReceiver
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    if (self = [super init]) {
+        [self mj_decode:aDecoder];
+    }
+    return self;
+}
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+    [self mj_encode:aCoder];
+}
+
+@end
